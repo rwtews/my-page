1aca37ff7c7c01d8fc408ba67cf1cd21525e6380
Robert García Morel <robertgarcia95@gmail.com>
2021-06-08 22:54:34 +0200

    Issue-3: Add carrouselComponent

---
+++ src/app/shared/carousel/carousel.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { NgbCarousel, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.scss'],
+})
+export class CarouselComponent {
+  @Input() images!: string[];
+  @Input() interval: number = 5000;
+  @ViewChild('carousel', { static: true }) carousel!: NgbCarousel;
+  
+  paused = false;
+  unpauseOnArrow = false;
+  pauseOnIndicator = false;
+  pauseOnHover = true;
+  pauseOnFocus = true;
+
+  togglePaused(): void {
+    if (this.paused) {
+      this.carousel.cycle();
+    } else {
+      this.carousel.pause();
+    }
+    this.paused = !this.paused;
+  }
+
+  onSlide(slideEvent: NgbSlideEvent): void {
+    if (this.unpauseOnArrow && slideEvent.paused &&
+      (slideEvent.source === NgbSlideEventSource.ARROW_LEFT || slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)) {
+      this.togglePaused();
+    }
+    if (this.pauseOnIndicator && !slideEvent.paused && slideEvent.source === NgbSlideEventSource.INDICATOR) {
+      this.togglePaused();
+    }
+  }
+}
